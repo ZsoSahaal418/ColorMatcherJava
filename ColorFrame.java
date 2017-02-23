fdae550239d9769caf78e7ba248171cece1bab25
ZsoSahaal418 <jhammer15.dixon@gmail.com>
2017-02-23 12:00:01 -0500

    Update ColorFrame.java
    Secondary class for later implementation

--- ColorFrame.java
+++ ColorFrame.java
@@ -1,15 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package colormatcherjava;
 
 import javax.swing.JFrame;
 
 /**
  *
- * @author John and Cass
+ * @author John
  */
 public class ColorFrame {
     
