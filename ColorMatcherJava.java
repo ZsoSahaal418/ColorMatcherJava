f12be24625c6bd67f7894f2c03e6494742376229
ZsoSahaal418 <jhammer15.dixon@gmail.com>
2017-02-23 12:00:44 -0500

    Update ColorMatcherJava.java

--- ColorMatcherJava.java
+++ ColorMatcherJava.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package colormatcherjava;
 
 import java.awt.BorderLayout;
@@ -32,7 +27,7 @@ import javax.swing.WindowConstants;
 
 /**
  *
- * @author John and Cass
+ * @author John
  */
 public class ColorMatcherJava {
 
